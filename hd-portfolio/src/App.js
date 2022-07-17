68fd5aa4847fbefb622717037472134d3ca981b0
Harrison Daniel <hd4n@outlook.com>
2022-07-17 19:05:40 -0400

    initial file structure and start to NavBar

---
+++ hd-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    // <div>
+    //   <h1 className="text-4xl font-signature">My Portfolio</h1>
+    // </div>,
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
