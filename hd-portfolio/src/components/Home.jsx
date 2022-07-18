619883c5e979754613723d4a253f32d4f7d0576d
Harrison Daniel <hd4n@outlook.com>
2022-07-17 23:54:29 -0400

    added gh-pages and script dependencies

--- hd-portfolio/src/components/Home.jsx
+++ hd-portfolio/src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            My Portfolio
             </h2>
           <p className="text-gray-500 py-4 max-w-md">
             Coming from the SaaS industry and working for small start-ups, I've seen all sides of creating and managing applications. I'm seeking new opportunities to pursue my passion for technology using the skills I've learned from the University of Richmond Bootcamp.
